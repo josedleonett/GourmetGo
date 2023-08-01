6050e59305c7efcc2170c4f514f3c8e8f1e52dc2
Leonardo Barrera <leonardobarrera397@gmail.com>
2023-08-01 01:01:07 -0300

    Creacion de proyecto

---
+++ src/main/java/com/proyectointegradorequipo3/proyectointegradorEquipo3/ProyectointegradorEquipo3Application.java
@@ -0,0 +1,13 @@
+package com.proyectointegradorequipo3.proyectointegradorEquipo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectointegradorEquipo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectointegradorEquipo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectointegradorequipo3/proyectointegradorEquipo3/ProyectointegradorEquipo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectointegradorequipo3.proyectointegradorEquipo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectointegradorEquipo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
