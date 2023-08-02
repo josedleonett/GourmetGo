3a765a0eea41dea3df4142e15a17ce85d86caef4
JosedLeonett <josed.95@gmail.com>
2023-08-02 18:41:40 -0300

    estructura del themeProvider agregado (theme.jsx)

--- FrontEnd/src/display/HeaderDisplay.jsx
+++ FrontEnd/src/display/HeaderDisplay.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import { makeStyles } from "@mui/material";
 
-const HeaderDisplay = ({props}) => {
+const useStyle = makeStyles({
+  header: {
+    background: "green",
+  },
+});
+
+const HeaderDisplay = ({ props }) => {
+  const style = useStyle();
+
   return (
-    <div>HeaderDisplay</div>
-  )
-}
+    <AppBar className={style.header} position="fixed" variant="dense">
+      <Container maxWidth="xl">
+        <Toolbar>
+          <Typography component="a" href="/" variant="h6">
+            GOURMET GO
+          </Typography>
+          <Button
+            variant="contained"
+            color="secondary"
+            sx={{ marginLeft: "auto" }}
+          >
+            LOG IN
+          </Button>
+          <Button variant="contained" color="secondary">
+            SIGN UP
+          </Button>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 
-export default HeaderDisplay
+export default HeaderDisplay;
