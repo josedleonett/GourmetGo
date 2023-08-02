725b6484e3713f40a27ca12f73029f09d6d85ad2
JosedLeonett <josed.95@gmail.com>
2023-08-02 20:19:50 -0300

    Co-authored-by: OctavioB17 <OctavioB17@users.noreply.github.com>

--- FrontEnd/src/display/HeaderDisplay.jsx
+++ FrontEnd/src/display/HeaderDisplay.jsx
@@ -4,36 +4,27 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
-import { makeStyles } from "@mui/material";
-
-const useStyle = makeStyles({
-  header: {
-    background: "green",
-  },
-});
+import { Box } from "@mui/material";
 
 const HeaderDisplay = ({ props }) => {
-  const style = useStyle();
-
   return (
-    <AppBar className={style.header} position="fixed" variant="dense">
-      <Container maxWidth="xl">
-        <Toolbar>
-          <Typography component="a" href="/" variant="h6">
-            GOURMET GO
-          </Typography>
-          <Button
-            variant="contained"
-            color="secondary"
-            sx={{ marginLeft: "auto" }}
-          >
-            LOG IN
-          </Button>
-          <Button variant="contained" color="secondary">
-            SIGN UP
-          </Button>
-        </Toolbar>
-      </Container>
+    <AppBar position="fixed" variant="dense">
+      <Toolbar sx={{ margin: 5 }}>
+        <Typography component="a" href="/" variant="h6">
+          GOURMET GO
+        </Typography>
+        <Button
+          variant="contained"
+          color="secondary"
+          sx={{ backgroundColor: "chocolate" }}
+        >
+          LOG IN
+        </Button>
+        
+        <Button variant="contained" color="secondary">
+          SIGN UP
+        </Button>
+      </Toolbar>
     </AppBar>
   );
 };
--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -6,6 +6,8 @@ import "./index.css";
 import NotFoundContainer from "./container/NotFoundContainer.jsx";
 import HomeContainer from "./container/HomeContainer.jsx";
 import { ContextProvider } from "./context/Global.context.jsx";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./utils/theme.jsx";
 
 const router = createBrowserRouter([
   {
@@ -25,7 +27,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ContextProvider>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </ContextProvider>
   </React.StrictMode>
 );
