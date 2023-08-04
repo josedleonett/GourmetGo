759864c752d4dcc1ba9ca1291ae85afc1a9c1a4c
Leonardo Barrera <leonardobarrera397@gmail.com>
2023-08-04 16:55:09 -0300

    AWS S3 config conection

---
+++ BackEnd/src/main/java/com/proyectointegradorequipo3/proyectointegradorEquipo3/services/S3Service.java
@@ -0,0 +1,9 @@
+package com.proyectointegradorequipo3.proyectointegradorEquipo3.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class S3Service {
+
+    private final static String BUCKET =  "gourmetgo";
+}
