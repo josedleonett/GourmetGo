b84633a9421ebdace2bd699cdb94eaca455c925c
JosedLeonett <josed.95@gmail.com>
2023-08-05 15:08:53 -0300

    added shuffleArray function.
    Now the product grid shows in random order

--- FrontEnd/src/container/CardProductGridContainer.jsx
+++ FrontEnd/src/container/CardProductGridContainer.jsx
@@ -9,12 +9,22 @@ const CardProductGridContainer = ({ list }) => {
     Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
       arr.slice(i * size, i * size + size)
     );
+
+    function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+        [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+      }
+      return array;
+    }
+
+    const shuffledListProduct = shuffleArray(list)
   
-    const chunkedList = chunk(list, 10)
-    console.log(chunkedList);
+    const chunkedProductList = chunk(shuffledListProduct, 10)
+    console.log(chunkedProductList);
 
   return (
-    <CardProductGridDisplay list={chunkedList}/>
+    <CardProductGridDisplay list={chunkedProductList}/>
   );
 };
 
