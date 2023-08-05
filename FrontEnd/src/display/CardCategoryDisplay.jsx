a81ecbf82cb7329008279d1f32f0f22756eb34b0
JosedLeonett <josed.95@gmail.com>
2023-08-05 07:54:06 -0300

    added flexDirection row for md displays

--- FrontEnd/src/display/CardCategoryDisplay.jsx
+++ FrontEnd/src/display/CardCategoryDisplay.jsx
@@ -3,28 +3,34 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
+import { Box, CardActionArea } from "@mui/material";
 
 const CardCategoryDisplay = ({ img, title, description }) => {
   return (
-    <Card >
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="240px"
-          image={img}
-          alt="green iguana"
-        />
-        <CardContent sx={{minHeight:153}}>
-          <Typography gutterBottom variant="h5">
-            {title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <>
+      <Card>
+        <CardActionArea>
+          <Box
+            sx={{ display: "flex", flexDirection: { lg: "column", md: "row" } }}
+          >
+            <CardMedia
+              component="img"
+              height="240px"
+              image={img}
+              alt="green iguana"
+            />
+            <CardContent sx={{ minHeight: 153 }}>
+              <Typography gutterBottom variant="h5">
+                {title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {description}
+              </Typography>
+            </CardContent>
+          </Box>
+        </CardActionArea>
+      </Card>
+    </>
   );
 };
 
