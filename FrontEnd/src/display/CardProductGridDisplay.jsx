6e2d9260d1371bc0fcad9c6b2eba6fbd9f88f0d4
JosedLeonett <josed.95@gmail.com>
2023-08-05 20:39:33 -0300

    added scroll up feeback when navigate through pagination
    rating props added

--- FrontEnd/src/display/CardProductGridDisplay.jsx
+++ FrontEnd/src/display/CardProductGridDisplay.jsx
@@ -1,18 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import CardProductContainer from "../container/CardProductContainer";
 import { Grid, Pagination, Container, Box } from "@mui/material";
 
 const CardProductGridDisplay = ({ list }) => {
   const [page, setPage] = useState(1);
+  const containerRef = useRef(null); // Paso 1: Crear una referencia
+
   const handleChange = (event, value) => {
     event.preventDefault();
     setPage(value);
+
+    // Scroll to start
+    containerRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
   };
 
   console.log(list);
 
   return (
-    <Container maxWidth="xl">
+    <Container maxWidth="xl" ref={containerRef}>
       <Grid container spacing={4} columns={{ lg: 5, md: 3, sm: 1 }}>
         {list[page - 1].map((item) => (
           <Grid item key={item.id} lg={1} md={1} sm={1}>
@@ -21,6 +29,8 @@ const CardProductGridDisplay = ({ list }) => {
                 img={item.bundleImage}
                 title={item.name}
                 description={item.description}
+                categoryList={item.categoryList}
+                rating={item.rating}
               />
             }
           </Grid>
