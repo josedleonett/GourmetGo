1af8603b51b46843c800523700acffc0dd52f1ce
JosedLeonett <josed.95@gmail.com>
2023-08-12 19:43:06 -0300

    router fixed

--- FrontEnd/src/pages/display/AdminDisplay.jsx
+++ FrontEnd/src/pages/display/AdminDisplay.jsx
@@ -11,7 +11,7 @@ const AdminDisplay = ({sidebarMenu}) => {
       <Box sx={{ display: "flex" }}>
 
         <AdminPanelDrawerContainer sidebarMenuList={sidebarMenu}/>
-
+        {}
         <AdminPanelMainContainer/>
 
         
