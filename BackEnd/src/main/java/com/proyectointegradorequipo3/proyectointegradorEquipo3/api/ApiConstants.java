8e59bdcad61d726e071f622dd3cc780a1d175409
Leonardo Barrera <leonardobarrera397@gmail.com>
2023-08-15 11:33:28 +0000

    Revert "Merge branch 'feature-back-image-resize' into 'main'"
    This reverts merge request !16

--- BackEnd/src/main/java/com/proyectointegradorequipo3/proyectointegradorEquipo3/api/ApiConstants.java
+++ BackEnd/src/main/java/com/proyectointegradorequipo3/proyectointegradorEquipo3/api/ApiConstants.java
@@ -3,8 +3,8 @@ package com.proyectointegradorequipo3.proyectointegradorEquipo3.api;
 public interface ApiConstants {
     String PLATE_URI = "/v1/plate/";
     String DRINK_URI = "/v1/drink/";
-    String CATEGORY_URI = "/v1/category/";
-    String BUNDLE_URI = "/v1/bundle/";
 
-    String ASSET_URI = "/v1/asset/";
+    String CATEGORY_URI = "/v1/category/";
+
+    String BUNDLE_URI = "/v1/bundle/";
 }
--- BackEnd/src/main/java/com/proyectointegradorequipo3/proyectointegradorEquipo3/services/S3Service.java
+++ BackEnd/src/main/java/com/proyectointegradorequipo3/proyectointegradorEquipo3/services/S3Service.java
@@ -7,18 +7,12 @@ import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.util.IOUtils;
 import com.proyectointegradorequipo3.proyectointegradorEquipo3.domain.vm.Asset;
-import org.imgscalr.Scalr;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.UUID;
 
 @Service
@@ -32,44 +26,19 @@ public class S3Service {
     //===================Save image===================//
     public String putObject(MultipartFile multipartFile) {
         String extension = StringUtils.getFilenameExtension(multipartFile.getOriginalFilename());
-        assert extension != null;
-        String format = getFormatForImageIO(extension);
         String key = String.format("%s.%s", UUID.randomUUID(), extension);
 
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentType(multipartFile.getContentType());
 
         try {
-            ByteArrayInputStream resizedImageStream = resizeImage(multipartFile, format);
-            metadata.setContentLength(resizedImageStream.available());
-            PutObjectRequest putObjectRequest = new PutObjectRequest(BUCKET, key, resizedImageStream, metadata);
+            PutObjectRequest putObjectRequest = new PutObjectRequest(BUCKET, key, multipartFile.getInputStream(), metadata);
             s3Client.putObject(putObjectRequest);
             return key;
-        } catch (Exception e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-
-    private String getFormatForImageIO(String extension) {
-        return switch (extension.toLowerCase()) {
-            case "jpeg", "jpg" -> "jpg";
-            case "png" -> "png";
-            case "gif" -> "gif";
-            default -> throw new IllegalArgumentException("Unsupported file format: " + extension);
-        };
-    }
-
-    public ByteArrayInputStream resizeImage(MultipartFile file, String format) throws Exception {
-        InputStream in = new ByteArrayInputStream(file.getBytes());
-        BufferedImage originalImage = ImageIO.read(in);
-
-        BufferedImage resizedImage = Scalr.resize(originalImage, Scalr.Method.QUALITY, Scalr.Mode.FIT_EXACT, 1280 , 800);
-
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        ImageIO.write(resizedImage, format, os);
-        return new ByteArrayInputStream(os.toByteArray());
-    }
-
 
 
     //===================Get image===================//
@@ -100,8 +69,5 @@ public class S3Service {
     public String getObjectUrl(String key){
         return String.format("https://%s.s3.amazonaws.com/%s", BUCKET, key);
     }
-
-
-
 }
 
