b570a4c27915d76ca819f962b05bcc906503236a
Leonardo Barrera <leonardobarrera397@gmail.com>
2023-08-28 08:16:50 -0300

    Plate and drink cache optimization

--- BackEnd/src/main/java/com/proyectointegradorequipo3/proyectointegradorEquipo3/services/impl/GetAllServiceImpl.java
+++ BackEnd/src/main/java/com/proyectointegradorequipo3/proyectointegradorEquipo3/services/impl/GetAllServiceImpl.java
@@ -3,8 +3,10 @@ package com.proyectointegradorequipo3.proyectointegradorEquipo3.services.impl;
 import com.proyectointegradorequipo3.proyectointegradorEquipo3.domain.Bundle;
 import com.proyectointegradorequipo3.proyectointegradorEquipo3.domain.dto.response.BundleDto;
 import com.proyectointegradorequipo3.proyectointegradorEquipo3.domain.dto.response.BundleForCardDto;
+import com.proyectointegradorequipo3.proyectointegradorEquipo3.domain.dto.response.DrinkDto;
 import com.proyectointegradorequipo3.proyectointegradorEquipo3.domain.dto.response.PlateDto;
 import com.proyectointegradorequipo3.proyectointegradorEquipo3.persistance.IBundleRepository;
+import com.proyectointegradorequipo3.proyectointegradorEquipo3.persistance.IDrinkRepository;
 import com.proyectointegradorequipo3.proyectointegradorEquipo3.persistance.IPlateRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,8 +22,11 @@ public class GetAllServiceImpl {
     private final IBundleRepository bundleRepository;
 
     private final PlateServiceImpl plateService;
-
     private final IPlateRepository plateRepository;
+
+
+    private final DrinkServiceImpl drinkService;
+    private final IDrinkRepository drinkRepository;
 
     public List<BundleDto> searchAllBundles() {
         List<Long> ids = bundleRepository.findAllIds();
@@ -36,5 +41,10 @@ public class GetAllServiceImpl {
     public List<PlateDto> searchAllPlates() {
         List<Long> ids = plateRepository.findAllIds();
         return ids.stream().map(plateService::searchPlateById).collect(Collectors.toList());
+    }
+
+    public List<DrinkDto> searchAllDrinks() {
+        List<Long> ids = drinkRepository.findAllIds();
+        return ids.stream().map(drinkService::searchDrinkById).collect(Collectors.toList());
     }
 }
