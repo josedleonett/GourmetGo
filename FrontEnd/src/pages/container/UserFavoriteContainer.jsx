9d3561c1614cb937576fab4890cae2b9e9433304
abril <abrilgomezp@outlook.com>
2023-09-21 03:42:18 -0300

    correcciones

--- FrontEnd/src/pages/container/UserFavoriteContainer.jsx
+++ FrontEnd/src/pages/container/UserFavoriteContainer.jsx
@@ -78,7 +78,7 @@ const UserFavoriteContainer = ({ accessToken }) => {
       {favorites.length >= 1 ? (
         <CardProductGridContainer list={favorites} setIdToDelete={setIdToDelete} />
       ) : isLoading ? (
-        <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+        <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "50vh" }}>
           <CircularProgress />
         </Box>
       ) : (
